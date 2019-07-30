b050b751c8e099622c6d905b222f43d836f58fcf
simone <simone.soncin.95@gmail.com>
2019-07-30 17:44:44 +0200

    prova button

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
 
 	function display(){
 		echo "hello ".$_POST["ciao"]; //stampa hello + contenuto textbox
+
 	}
 ?>
 <html>
@@ -14,16 +15,29 @@
   		<link href="./style.css" rel="stylesheet" type="text/css">
  	</head>
  	<body>
+		 <noscript>
+    	Javascript is not enabled. Please, enable it!
+		</noscript>
 		<table>
 			<form method="post" action="index.php"> <!-- la pagina php da chiamare (esegue refresh) -->
 				<tr>
 					debug 
 					<input type="text" name="ciao">
 					<div>
-						<input type="submit" name="invia" value="ok">
+						<input type="submit" name="invia" value="bottone submit">
+						<br>
+						<input type="button" type="submit" name="invia" value="bottone" onclick="myFunction()">
 					</div>
 				</tr>
 			</form>
 		</table>
 	</body>
+	<script type="text/javascript">
+
+	function myFunction()
+	{
+		alert("I am an alert box!");
+	}
+	</script>
 </html>
+
