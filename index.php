5b712a24a66798a0e21bbd526ed1f179f69633cf
simone <simone.soncin.95@gmail.com>
2019-07-30 17:28:17 +0200

    prova submit

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
-<?php include('server.php')
+<?php include('server.php');
 
+	if(isset($_POST['invia'])) //premendo invia
+	{
+		display();
+	} 
+
+	function display(){
+		echo "hello ".$_POST["ciao"]; //stampa hello + contenuto textbox
+	}
 ?>
 <html>
 	<head>
@@ -7,10 +15,15 @@
  	</head>
  	<body>
 		<table>
-			<tr>
-				debug 
-				<input type="text" name="ciao">
-			</tr>
+			<form method="post" action="index.php"> <!-- la pagina php da chiamare (esegue refresh) -->
+				<tr>
+					debug 
+					<input type="text" name="ciao">
+					<div>
+						<input type="submit" name="invia" value="ok">
+					</div>
+				</tr>
+			</form>
 		</table>
 	</body>
 </html>
